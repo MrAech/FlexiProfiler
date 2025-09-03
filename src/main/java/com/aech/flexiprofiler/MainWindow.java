5589564b2f81b4bb34af60e3631166cff04ef825
MrAech <happy2006sharma@gmail.com>
2025-09-03 22:20:32 +0530

    -(chore)Seperated TopBar section So it can be used in other files

--- src/main/java/com/aech/flexiprofiler/MainWindow.java
+++ src/main/java/com/aech/flexiprofiler/MainWindow.java
@@ -6,14 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class MainWindow extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/aech/flexiprofiler/Dashboard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/aech/flexiprofiler/fxmls/Dashboard.fxml"));
         Scene scene = new Scene(root, 1280, 780);
-        scene.getStylesheets().add(getClass().getResource("/com/aech/flexiprofiler/styles/dashboard.css").toExternalForm());
+//        scene.getStylesheets().add(getClass().getResource("/com/aech/flexiprofiler/styles/dashboard.css").toExternalForm());
         primaryStage.setTitle("FlexiProfile — Dataset Import Dashboard");
         primaryStage.setScene(scene);
         primaryStage.show();
